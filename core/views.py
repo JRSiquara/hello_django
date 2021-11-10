60c2defc9b7185c0d666ebb9d9494a09835d3bcb
Drago <jrsiquara@gmail.com>
2021-11-10 14:25:44 -0300

    Atualizando execircio com a soma de dois valores

--- core/views.py
+++ core/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
-    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+# def hello(request, nome, idade):
+#     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+     soma_n1n2 = num1 + num2
+     return HttpResponse('<h1>Soma {} + {} = {}</h1>'.format(num1, num2, soma_n1n2))
