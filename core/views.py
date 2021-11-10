9a06be35c54d684da148ce1f738d433f9a9e08ac
Drago <jrsiquara@gmail.com>
2021-11-10 14:41:55 -0300

    Atualizando exercicio com a soma de dois valores

--- core/views.py
+++ core/views.py
@@ -8,3 +8,7 @@ from django.shortcuts import render, HttpResponse
 def soma(request, num1, num2):
      soma_n1n2 = num1 + num2
      return HttpResponse('<h1>Soma {} + {} = {}</h1>'.format(num1, num2, soma_n1n2))
+
+def multiplicacao(request, num1, num2):
+     soma_n1n2 = num1 * num2
+     return HttpResponse('<h1>Multiplicação {} + {} = {}</h1>'.format(num1, num2, soma_n1n2))
